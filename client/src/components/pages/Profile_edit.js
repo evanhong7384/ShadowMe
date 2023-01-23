1396192b7b21c1af496b2bad4eb110c615f4fc21
Evan Hong <evanhong@dhcp-10-29-9-105.dyn.mit.edu>
2023-01-23 11:04:10 -0500

    profile edits

---
+++ client/src/components/pages/Profile_edit.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
+//const User = require("./models/user");
+import "../../utilities.css";
+import "./Profile_edit.css";
+import { post } from "../../utilities";
+//import "./Skeleton.css";
+
+//TODO: REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "847255392628-tdlh0r7m9s9vq6isrb54ind9k7sk34hg.apps.googleusercontent.com";
+
+
+ const saveInfo = () =>{
+  
+  post('/api/pfedit',{name: document.getElementById("Name"),
+  institution: document.getElementById("Institutions"),
+  resume: document.getElementById("Resume"),
+  linkedin: document.getElementById("Linkedin"),
+  location: document.getElementById("Location"),
+  bio: document.getElementById("Bio")}).then((response)=>{
+  alert(response);
+  
+ })   
+}
+
+const Profile_edit = () => {
+  return (
+    <form>
+    <div className="personal_info">
+        <label for="Name">Name:
+        <input type="text" id="Name" name="Name" />
+        </label>
+
+        <label for="Institutions">Institutions:
+        <input type="text" id="Institutions" name="Institution"/>
+        </label>
+
+        <label for="Resume">Resume:
+        <input type="text" id="Resume" name="Resume"/>
+        </label>
+
+        <label for="Linkedin">Linkedin:
+        <input type="text" id="Linkedin" name="Linkedin"/>
+        </label>
+
+        <label for="Location">Location:
+        <input type="text" id="Location" name="Location"/>
+        </label>
+
+        <label for="Bio">Bio:
+        <input type="text" id="Bio" name="Bio"/>
+        </label>
+        
+
+        <div> 
+          <button id='submit' type="submit"  onClick={saveInfo}>save</button>
+        </div>
+        
+        
+        
+    </div>
+    </form>
+    
+
+    
+
+
+    
+  );
+};
+
+export default Profile_edit;
