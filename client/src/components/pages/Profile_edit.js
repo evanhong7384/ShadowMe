7d0a8a2c7e687cf4d4550520019f144dafc842f9
Evan Hong <evanhong@Evans-MacBook-Pro.local>
2023-01-24 13:03:14 -0500

    just the login bar at the top of the page

--- client/src/components/pages/Profile_edit.js
+++ client/src/components/pages/Profile_edit.js
@@ -28,8 +28,21 @@ const GOOGLE_CLIENT_ID = "847255392628-tdlh0r7m9s9vq6isrb54ind9k7sk34hg.apps.goo
  })   
 }
 
-const Profile_edit = () => {
+const Profile_edit = (userId, handleLogin, handleLogout) => {
   return (
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      {userId ? (
+        <button
+          onClick={() => {
+            googleLogout();
+            handleLogout();
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
+      )}
     <form>
     <div className="personal_info">
         <label for="Name">Name:
@@ -65,6 +78,7 @@ const Profile_edit = () => {
         
     </div>
     </form>
+    </GoogleOAuthProvider>
     
 
     
