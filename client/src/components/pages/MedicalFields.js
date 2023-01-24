c352179e7fa819dd4ab177401cbfbc0fd2d3829e
William McTeigue <mcteigue@mit.edu>
2023-01-23 22:02:36 -0500

    Checkboxes for medical field and task interests

--- client/src/components/pages/MedicalFields.js
+++ client/src/components/pages/MedicalFields.js
@@ -1,6 +1,6 @@
 export const medicalFields = [
   {
-    name: "Surgery"
+    name: "General surgery"
   },
   {
     name: "Pediatrics"
@@ -19,5 +19,62 @@ export const medicalFields = [
   },
   {
     name: "Radiology"
+  },
+  {
+    name: "Orthopedics"
+  },
+  {
+    name: "Physical therapy"
+  },
+  {
+    name: "Psychiatry"
+  },
+  {
+    name: "Dermatology"
+  },
+  {
+    name: "Obstetrics and gynaecology"
+  },
+  {
+    name: "Anesthesiology"
+  },
+  {
+    name: "Public health"
+  },
+  {
+    name: "Ophthalmology"
+  },
+  {
+    name: "Urology"
+  },
+  {
+    name: "Oncology"
+  },
+  {
+    name: "Podiatry"
+  },
+  {
+    name: "Cardiology"
+  },
+  {
+    name: "Dentistry"
+  },
+  {
+    name: "Plastic surgery"
+  }
+];
+
+export const medicalTasks = [
+  {
+    name: "Record Keeping"
+  },
+  {
+    name: "Undergraduate Research"
+  },
+  {
+    name: "Patient Care"
+  },
+  {
+    name: "Patient History"
   }
 ];
