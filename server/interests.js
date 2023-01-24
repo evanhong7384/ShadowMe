91e45d58f7b12f89964eee326bff4842b2409a20
Your Name <you@example.com>
2023-01-24 14:52:50 -0500

    make interests.js return Interests component

--- server/interests.js
+++ server/interests.js
@@ -18,13 +18,16 @@ const auth = require("./auth");
 // api endpoints: all these paths will be prefixed with "/api/"
 const router = express.Router();
 
-
+// include Interests.js component
+const Interests = require("./interests");
 
 //initialize socket
 const socketManager = require("./server-socket");
 
 router.all("*", (req, res) => {
-  res.send("welcome to the interests page basic!");
+  //res.send("welcome to the interests page basic!");
+  // return the Interests component 
+  res.send(Interests);  
 });
 
 module.exports = router;
