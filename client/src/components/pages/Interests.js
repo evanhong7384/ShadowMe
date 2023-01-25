87feb702cc82412da2a31c10e664fd8e94794ec0
Your Name <you@example.com>
2023-01-25 13:33:54 -0500

    add second set of checkboxes to interests page, remove commented out block of code

--- client/src/components/pages/Interests.js
+++ client/src/components/pages/Interests.js
@@ -30,7 +30,7 @@ const Interests = () => {
 
   return (
     <>
-      <div className="Interests">
+      <div className="Fields">
         <h3>What Fields Are You Interested In?</h3>
         <ul className="medicalField-list">
           {medicalFields.map(({ name }, index) => {
@@ -42,6 +42,7 @@ const Interests = () => {
                   name={name}
                   value={name}
                   checked={checkedStateFields[index]}
+                  onChange={() => firstSetOfCheckboxes(index)}
                 />
                 <label htmlFor={`checked-Fields-${index}`}>{name}</label>
               </li>
@@ -49,96 +50,32 @@ const Interests = () => {
           })}
         </ul>
       </div>
+      <div className="Interests">
+        <h3>What Tasks Are You Most Interested In Helping With?</h3>
+        <ul className="medicalTask-list">
+          {medicalTasks.map(({ name }, index) => {
+            return (
+              <li key={index}>
+                <div className="medicalTask-list-item">
+                  <div className="left-section">
+                    <input
+                      type="checkbox"
+                      id={`checkbox-tasks-${index}`}
+                      name={name}
+                      value={name}
+                      checked={checkedStateTasks[index]}
+                      onChange={() => secondSetOfCheckboxes(index)}
+                    />
+                    <label htmlFor={`checkbox-tasks-${index}`}>{name}</label>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
-
-/*
-import { medicalFields } from "./MedicalFields.js";
-import "./Medicalfieldsstyles.css";
-
-const Interests = () => {
-
-    const [checkedStateFields, setCheckedStateFields] = useState(
-        new Array(medicalFields.length).fill(false)
-      );
-    
-      const [checkedStateTasks, setCheckedStateTasks] = useState(
-        new Array(medicalTasks.length).fill(false)
-      );
-    
-      const firstSetOfCheckboxes = (position) => {
-        const updatedCheckedStateFields = checkedStateFields.map((item, index) =>
-          index === position ? !item : item
-        );
-        setCheckedStateFields(updatedCheckedStateFields);
-      };
-    
-      const secondSetOfCheckboxes = (position) => {
-        const updatedCheckedStateTasks = checkedStateTasks.map((item, index) =>
-          index === position ? !item : item
-        );
-        setCheckedStateTasks(updatedCheckedStateTasks);
-      };
-
-    
-      return (
-        <>
-          <div className="Interests">
-            <h3>What Fields Are You Interested In?</h3>
-            <ul className="medicalField-list">
-              {medicalFields.map(({ name }, index) => {
-                return (
-                  <li key={index}>
-                    <div className="medicalField-list-item">
-                      <div className="left-section">
-                        <input
-                          type="checkbox"
-                          id={`checkbox-Fields-${index}`}
-                          name={name}
-                          value={name}
-                          checked={checkedStateFields[index]}
-                          onChange={() => firstSetOfCheckboxes(index)}
-                        />
-                        <label htmlFor={`checkbox-Fields-${index}`}>{name}</label>
-                      </div>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className="Interests">
-            <h3>What Tasks Are You Most Interested In Helping With?</h3>
-            <ul className="medicalTask-list">
-              {medicalTasks.map(({ name }, index) => {
-                return (
-                  <li key={index}>
-                    <div className="medicalTask-list-item">
-                      <div className="left-section">
-                        <input
-                          type="checkbox"
-                          id={`checkbox-tasks-${index}`}
-                          name={name}
-                          value={name}
-                          checked={checkedStateTasks[index]}
-                          onChange={() => secondSetOfCheckboxes(index)}
-                        />
-                        <label htmlFor={`checkbox-tasks-${index}`}>{name}</label>
-                      </div>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div>
-        Other Interests: 
-        <input />
-      </div>
-        </>
-      );
-    };
-*/
 
 export default Interests;
