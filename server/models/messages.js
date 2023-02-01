caadfc68166b301eb0ba9c81cf244643f65b621f
Your Name <you@example.com>
2023-02-01 10:35:34 -0500

    add messages schema

---
+++ server/models/messages.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const MessageSchema = new mongoose.Schema({
+  messageText: String,
+  googleidFrom: String,
+  googleidTo: String,
+  sentTime: Date,
+  readTime: Date,
+});
+
+// compile model from schema
+module.exports = mongoose.model("message", MessageSchema);
